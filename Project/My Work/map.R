2723953dcb612f3e991f66cdc8265ee812f83555
ravi-prakash1907 <1907prakash.ravi@gmail.com>
2020-03-04 01:40:27 +0530

    Plotting on World-Map

--- Project/My Work/map.R
+++ Project/My Work/map.R
@@ -1,6 +1,9 @@
 # visualization on map
-library(countrycode)
 library(stringr)
+library(countrycode)
+library(ggplot2)
+library(ggmap)
+library(maps)
 
 # Setting the working directory
 setwd("/home/ravi/Documents/Data-Science/Project/My Work/")
@@ -13,6 +16,7 @@ Countries <- levels(Confirmed$Country.Region)
 # removing Cruise from country list
 Countries = Countries[which(str_detect(Countries, "Diamond Princess cruise ship", negate = T))]
 
+# generating country code
 country.code = countrycode(
   sourcevar = Countries,
   origin = "country.name",
@@ -22,11 +26,50 @@ country.code = countrycode(
 
 country.code   # this can be easily plotted on graph
 
-
+iso.expand(country.code, regex=TRUE)
 
 ####################
 #  TO PLOT STATES (of a country)
 
+## https://eriqande.github.io/rep-res-web/lectures/making-maps-with-R.html
+
+
+
+map.world <- map_data("world") # usa map, similarl;,     "world" give high res world map centered on the Pacific Ocean 
+
+to_append <- data.frame(
+  rank = 1:length(country.code),
+  country = Countries
+)
+
+map.world_joined <- left_join(map.world, to_append, by = c('region' = 'country'))
+
+map.world_joined <- map.world_joined %>% mutate(fill_flg = ifelse(is.na(rank),F,T))
+head(map.world_joined)
+
+
+# countries/locations affected by coronavirus
+ggplot() +
+  geom_polygon(data = map.world_joined, aes(x = long, y = lat, group = group, fill = fill_flg)) +
+  geom_point(data = Confirmed, aes(x = Long, y = Lat), color = "black") +
+  scale_fill_manual(values = c("#CCCCCC","#e60000")) +
+  labs(title = 'Countries and their provinces, affected by "COVID-19"',
+       subtitle = "Source: https://github.com/ravi-prakash1907/Data-Science",
+       caption = "Plot by @ravi") +
+  
+  theme(text = element_text(family = "Gill Sans", color = "#FFFFFF")
+        ,panel.background = element_rect(fill = "#444444")
+        ,plot.background = element_rect(fill = "#444444")
+        ,panel.grid = element_blank()
+        ,plot.title = element_text(size = 30)
+        ,plot.subtitle = element_text(size = 10)
+        ,axis.text = element_blank()
+        ,axis.title = element_blank()
+        ,axis.ticks = element_blank()
+        ,legend.position = "none"
+  )
+
+ggsave("PLOTS/affected locations.png", width = 16, height = 9, scale = 1)
 
 
 
