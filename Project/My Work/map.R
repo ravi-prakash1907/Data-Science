ed3417272ccf1721c92346f8342ee70b3734336e
ravi-prakash1907 <1907prakash.ravi@gmail.com>
2020-03-03 23:57:59 +0530

    Generating Country Code

--- Project/My Work/map.R
+++ Project/My Work/map.R
@@ -1,5 +1,32 @@
 # visualization on map
+library(countrycode)
+library(stringr)
 
+# Setting the working directory
+setwd("/home/ravi/Documents/Data-Science/Project/My Work/")
+
+
+## TO PLOT COUNTRIES
+#generating List of countries
 Confirmed <- read.csv("cleaned/time_series_19-covid-Confirmed.csv")
 Countries <- levels(Confirmed$Country.Region)
+# removing Cruise from country list
+Countries = Countries[which(str_detect(Countries, "Diamond Princess cruise ship", negate = T))]
+
+country.code = countrycode(
+  sourcevar = Countries,
+  origin = "country.name",
+  destination = "iso2c",
+  nomatch = NULL
+)
+
+country.code   # this can be easily plotted on graph
+
+
+
+####################
+#  TO PLOT STATES (of a country)
+
+
+
 
