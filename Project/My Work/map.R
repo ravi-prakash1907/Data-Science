1bdf2647f8b7721646b1007c3da761aa19ef290e
ravi-prakash1907 <1907prakash.ravi@gmail.com>
2020-03-09 02:04:49 +0530

    Updated for visualizations

--- Project/My Work/map.R
+++ Project/My Work/map.R
@@ -6,69 +6,34 @@ library(rlist)
 library(ggplot2)
 library(ggmap)
 library(maps)
-library(ImageMagick) ##  read.gif(), write.gif()
 
 # Setting the working directory
 setwd("/home/ravi/Documents/Data-Science/Project/My Work/")
 
 
 ## TO PLOT COUNTRIES
-#generating List of countries
+# generating List of countries
 ever.Affected <- read.csv("cleaned/ever.Affected.csv")
 still.Affected <- read.csv("cleaned/still.Affected.csv")
 
 
 
 #####################################################################
-#                             Functions                             #
+#                             Function                              #
 #####################################################################
 
-# to generate maps
-map_generator = function(df1, df2, Date) {
-  
-  get(df1) -> to_append
-  get(df2) -> this.affected.Today
-  
-  map.world <- map_data("world") 
-  
-  map.world_joined <- left_join(map.world, to_append, by = c('region' = 'country'))
-  map.world_joined <- map.world_joined %>% mutate(fill_flg = ifelse(is.na(rank),F,T))
-  
-  date.today = paste("Date:", Date, sep = " ")
-  
-  # countries/locations affected by coronavirus
-  ggplot() +
-    geom_polygon(data = map.world_joined, aes(x = long, y = lat, group = group, fill = fill_flg)) +   #      , alpha = 0.4
-    geom_point(data = this.affected.Today, aes(x = Long, y = Lat), color = "black") +
-    scale_fill_manual(values = c("#cccccc","#e60000")) +
-    scale_alpha_manual(values = c(0.1, 0.1)) +
-    labs(title = '"COVID-19"',
-         subtitle = date.today,
-         caption = "Plot by @ravi") +
-    
-    theme(text = element_text(family = "Gill Sans", color = "#FFFFFF")
-          ,panel.background = element_rect(fill = "#444444")
-          ,plot.background = element_rect(fill = "#444444")
-          ,panel.grid = element_blank()
-          ,plot.title = element_text(size = 25)
-          ,plot.subtitle = element_text(size = 15, color = "red")
-          ,plot.caption = element_text(size = 10)
-          ,axis.text = element_blank()
-          ,axis.title = element_blank()
-          ,axis.ticks = element_blank()
-          ,legend.position = "none"
-    )
-  
-}
-
 # MAIN : for visualization
-visualize_on_map <- function(df) {
-  get("ever.Affected") -> Affected
+visualize_on_map <- function(df, Path) {
+  get(df) -> Affected
   
   ##########################################
   
   # storing column names
   columns <- colnames(Affected)
+  
+  day = 1:(length(columns)-4)
+  d = as.Date("20/01/2020", format(c("%d/%m/%Y")))
+  date = as.character((day + d), format(c("%d/%m/%Y")))
   
   # storing data frames datewise
   for (today in 4+seq_along(columns)) {
@@ -87,12 +52,12 @@ visualize_on_map <- function(df) {
   # generating plots, datewise
   i = 1
   while(T) {
-    if(i>length(myList)){
+    if(i>length(myList)){ # length(myList)
       print("Stoppppppp!!!!!!!!!!!!!!!!")
       break
     }
     
-    affected.Today = as.data.frame(myList[i])  # myList[1]
+    affected.Today = as.data.frame(myList[i])
     row.names(affected.Today) <- NULL
     
     Countries <- factor(as.character(affected.Today$Country.Region))
@@ -103,32 +68,53 @@ visualize_on_map <- function(df) {
     )
     
     
-    map_generator("to_append", "affected.Today", columns[i+4])
+    ############################################################################################
+    
+    # getting world map
+    map.world <- map_data("world") 
+    
+    map.world_joined <- left_join(map.world, to_append, by = c('region' = 'country'))
+    map.world_joined <- map.world_joined %>% mutate(fill_flg = ifelse(is.na(rank),F,T))
+    
+    # countries/locations affected by coronavirus
+    ggplot() +
+      geom_polygon(data = map.world_joined, aes(x = long, y = lat, group = group, fill = fill_flg), color = "#252525") +
+      geom_point(data = affected.Today, aes(x = Long, y = Lat), color = "white") +
+      scale_fill_manual(values = c("#414141","#af0404")) +
+      labs(title = "2019-nCoV",
+           subtitle = date[i],
+           caption = "by @ravi") +
+      
+      theme(text = element_text(family = "Gill Sans", color = "#FFFFFF")
+            ,panel.background = element_rect(fill = "#252525")
+            ,plot.background = element_rect(fill = "#252525")
+            ,panel.grid = element_blank()
+            ,plot.title = element_text(size = 20, face = "bold", color = "#ff0000", hjust = 0.5)
+            ,plot.subtitle = element_text(size = 25, family = "Courier", face = "bold", hjust = 0.5)
+            ,plot.caption = element_text(size = 15, family = "Bookman", face = "italic", hjust = 0.9)
+            ,axis.text = element_blank()
+            ,axis.title = element_blank()
+            ,axis.ticks = element_blank()
+            ,legend.position = "none"
+      )
+    
+    ##########################################################################################
+    
+    
     pic = paste("pic",i, sep = "")
     imageName = paste(pic,"png", sep = ".")
     
-    ggsave(path = "PLOTS/pngs", filename = imageName, width = 16, height = 9, scale = 1)
+    ggsave(path = Path, filename = imageName, width = 16, height = 9, scale = 1)
     i = i+1
   }
 }
 
 #####################################################################
 
-
-visualize_on_map("ever.Affected")
-visualize_on_map("still.Affected")
-
-system("cd PLOTS")
-system("PLOTS/pngs")
-
-# Converting .png files in one .gif image using ImageMagick
-system("convert -delay 80 PLOTS/pngs/*.png PLOTS/gifs/ever.Affected.gif")
-# Remove .png files from working directory
-#file.remove(list.files(pattern=".png"))
-
+visualize_on_map("ever.Affected", "PLOTS/pngs/ever")
+visualize_on_map("still.Affected", "PLOTS/pngs/still")
 
 ####      TASKS
-# 1) GIF
 # 2) % change ---->  req. datasets (chapter 2)
 
 
@@ -137,28 +123,6 @@ system("convert -delay 80 PLOTS/pngs/*.png PLOTS/gifs/ever.Affected.gif")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-###################################################################
-#saveGIF({
-  #for (i in 1:10){  
-    #a <- ggplot(df[1:i,], aes(a,b)) + 
-      #geom_point() + 
-      #scale_x_continuous(limits=c(0,10)) +
-      #scale_y_continuous(limits=c(0,10))
-    #print(a)}
-#}, interval = .2, movie.name="test.gif")
 
 
 
--- Project/My Work/visualizer.R
+++ Project/My Work/visualizer.R
@@ -4,8 +4,9 @@
 setwd("/home/ravi/Documents/Data-Science/Project/My Work/")
 
 #library(ggplot2)   # gg menns ->   grammer of graphics
-library(tidyverse)  # includes ggplot2
+library(tidyverse)
 library(lattice)
+library(plyr) # used to calculate the average weight of each group 
 
 #------------------------------------------------------------------------#
 
@@ -31,70 +32,6 @@ Four.dataset.dateWise = read.csv("ready_to_use/COVID-19/FOUR/Four_dataset_dateWi
 
 #---------------------------------------------------------------#
 ### Functions ###
-
-# Recovery/Death over Confirm cases
-plot.recovered.in = function(cName, yesORno = FALSE) {
-  
-  
-  countryWise.bulk.summary.country = countryWise.bulk.summary[
-                                        which(str_detect(countryWise.bulk.summary$Country,
-                                                         cName,
-                                                         negate = yesORno)),
-                                        ]
-  
-  ############################################
-  
-  d <- countryWise.bulk.summary.country %>% 
-    as_tibble()
-  
-  d_ends <- countryWise.bulk.summary.country %>% 
-    group_by(Country) %>% 
-    top_n(1, Confirmed) %>% 
-    pull(Recovered)    # col can be changed
-  
-  d %>% 
-    ggplot(aes(Confirmed, Recovered, color = Country)) +
-    geom_line(size = 2, alpha = .8) +
-    theme_minimal() +
-    scale_x_continuous(expand = c(0, 0)) +
-    scale_y_continuous(sec.axis = sec_axis(~ ., breaks = d_ends)) +
-    ggtitle("Few(specify type of such countries) reporting more recovered for the confirmed cases",
-            subtitle = "Based on the dataset of given country") +
-    labs(x = "Confirmed Cases", y = "Recovered", caption = "Plot by @ravi")
-  
-}
-
-
-plot.deaths.in = function(cName, yesORno = FALSE) {
-  
-  
-  countryWise.bulk.summary.country = countryWise.bulk.summary[
-    which(str_detect(countryWise.bulk.summary$Country,
-                     cName,
-                     negate = yesORno)),
-    ]
-  
-  ############################################
-  
-  d <- countryWise.bulk.summary.country %>% 
-    as_tibble()
-  
-  d_ends <- countryWise.bulk.summary.country %>% 
-    group_by(Country) %>% 
-    top_n(1, Confirmed) %>% 
-    pull(Deaths)    # col can be changed
-  
-  d %>% 
-    ggplot(aes(Confirmed, Deaths, color = Country)) +
-    geom_line(size = 2, alpha = .8) +
-    theme_minimal() +
-    scale_x_continuous(expand = c(0, 0)) +
-    scale_y_continuous(sec.axis = sec_axis(~ ., breaks = d_ends)) +
-    ggtitle("Few(specify type of such countries) reporting more deaths for the confirmed cases",
-            subtitle = "Based on the dataset of given country") +
-    labs(x = "Confirmed Cases", y = "Deaths", caption = "Plot by @ravi")
-  
-}
 
 
 # Cases daily
@@ -134,7 +71,6 @@ confirmed.till.date = function(cName, yesORno = FALSE) {
     labs(x = "Dates", y = "Confirmed", caption = "Plot by @ravi")
   
 }
-
 
 deaths.till.date = function(cName, yesORno = FALSE) {
   
@@ -206,6 +142,7 @@ recovery.till.date = function(cName, yesORno = FALSE) {
   
 }
 
+
 # for world data
 till.date = function(yAxis, dataSet, cName = c("Hubei", "World", "China", "Diamond Princess"), yesORno = FALSE) {
   
@@ -243,13 +180,6 @@ till.date = function(yAxis, dataSet, cName = c("Hubei", "World", "China", "Diamo
   
 }
 
-
-
-
-
-
-
-
 till.date.Confirmed = function(lName = c("Hubei", "World", "China", "Diamond Princess"), yesORno = FALSE) {
   
   
@@ -284,9 +214,6 @@ till.date.Confirmed = function(lName = c("Hubei", "World", "China", "Diamond Pri
     labs(x = "Dates", y = "Confirmed", caption = "Plot by @ravi")
   
 }
-
-
-
 
 till.date.Deaths = function(lName = c("Hubei", "World", "China", "Diamond Princess"), yesORno = FALSE) {
   
@@ -323,9 +250,6 @@ till.date.Deaths = function(lName = c("Hubei", "World", "China", "Diamond Prince
   
 }
 
-
-
-
 till.date.Recovered = function(lName = c("Hubei", "World", "China", "Diamond Princess"), yesORno = FALSE) {
   
   
@@ -360,51 +284,15 @@ till.date.Recovered = function(lName = c("Hubei", "World", "China", "Diamond Pri
     labs(x = "Dates", y = "Recovered", caption = "Plot by @ravi")
   
 }
-
-
-
-
-
-
-
-
-
 #---------------------------------------------------------------#
 
-View(countryWise.bulk.summary)
 
-#################################
 #######  VISUALIZATIONS  ########
-#################################
-
-bulk.summary = read.csv("ready_to_use/COVID-19/Mixed/bulk_summary.csv")
-#  Plotting state VS deaths --> country wise
-ggplot(bulk.summary, aes(x = States, y = Confirmed, color = Country)) +
-  geom_jitter(alpha = 0.7) +
-  scale_y_continuous(breaks=seq(0,1500,100))   # sets y's range from 0 to 1500, with teak of 51
 
 
 #################################
 #########  Line Plots  ##########
 #################################
-
-## Visualizing 'deaths over confirmed' COVID-19 cases
-# for single country
-plot.recovered.in("Hong Kong")
-plot.deaths.in("China")
-
-# for multiple country
-plot.recovered.in(c("Hong Kong", "Italy", "South Korea", "Iran"))
-plot.deaths.in(c("Hong Kong", "Italy", "South Korea", "Iran"))
-
-# esle then the specified country
-plot.recovered.in("China", TRUE)
-plot.deaths.in("China", TRUE)
-
-# esle then the specified countries
-countryList = levels(countryWise.bulk.summary$Country)
-# plot.recovered.in(c("Vietnam", "US", "Taiwan"), TRUE)  # not working
-# plot.deaths.in(c("China", "Hong Kong", "Italy", "South Korea", "Iran"), T)  # not working
 
 
 ############### day wise
@@ -428,13 +316,59 @@ till.date.Confirmed(c("Hubei", "China"))
 till.date.Deaths("Hubei", T)
 till.date.Recovered()
 
-# Histigrams
-### plotting  ###
-# devision of survivers in each class
-# ggplot(train, aes(x = Pclass, fill = factor(Survived))) +
-#  geom_histogram(binwidth = 0.5) +
-#  xlab("Pclass") +
-#  ylab("Total Count") +
-#  labs(fill = "Survived")
+###########################################################################
+
+
+
+
+
+
+
+#################################
+#########  Histigrams  ##########
+#################################
+
+# 1) Active / Closed cases  (World)
+View(Four.dataset.locationWise)
+world = Four.dataset.locationWise[which(str_detect(Four.dataset.locationWise$Location, "World")),]
+world.df = data.frame(
+  Day = c(rep(world$Day, 2)),
+  Date = c(rep(as.Date(world$Date), 2)),
+  "Case Status" = factor(c(rep("Active", nrow(world)), rep("Closed", nrow(world))), levels = c("Active", "Closed")),
+  Numbers = c(world$Active.Cases, world$Closed.Cases)
+)
+
+# Change line type
+mean.caseWise <- ddply(world.df, "Case.Status", summarise, grp.mean=mean(Numbers))
+head(mean.caseWise)
+
+ggplot(world.df, aes(x=Numbers, color=Case.Status)) +
+  geom_histogram(binwidth = 2500, fill="white", alpha=0.5, position="dodge") +
+  geom_vline(data=mean.caseWise, aes(xintercept = grp.mean, color = Case.Status), linetype = "dashed") +
+  theme(legend.position = "top") +scale_color_grey() + theme_classic() +
+  theme(legend.position="top")
+
+
+ggplot(world.df, aes(x=Numbers, color=Case.Status)) +
+  geom_histogram(binwidth = 2500, fill = "white", position = "dodge") +
+  geom_vline(data=mean.caseWise, aes(xintercept = grp.mean, color = Case.Status),
+             linetype = "dashed") +
+  scale_color_brewer(palette="Paired") +
+  theme_classic()+theme(legend.position="top")
+
+
+ggplot(world.df, aes(x=Numbers, color=Case.Status, fill=Case.Status)) +
+  geom_histogram(binwidth = 3500, aes(y=..density..), position="identity", alpha=0.5)+
+  geom_density(alpha=0.6)+
+  geom_vline(data=mu, aes(xintercept=grp.mean, color=Case.Status),
+             linetype="dashed")+
+  scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9"))+
+  scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9"))+
+  labs(title="Cases histogram plot",x="Cases", y = "Density")+
+  theme_classic()
+
+
+
+
 
 
